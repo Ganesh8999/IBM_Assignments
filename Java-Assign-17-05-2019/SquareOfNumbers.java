4950285730cb85fbf114b938dbb6c456b8eccbe1
Ganesh Singh <singhganesh571@gmail.com>
2019-05-18 11:31:34 +0530

    Add files via upload

--- Java-Assign-17-05-2019/SquareOfNumbers.java
+++ Java-Assign-17-05-2019/SquareOfNumbers.java
@@ -11,13 +11,33 @@ class SquareOfNumbers{
 		Scanner sc = new Scanner(System.in);
 		String again;
 		do{
+
+			ArrayList<Integer> list = new ArrayList<>();
 			
-			System.out.println("How many numbers want to add");
+			System.out.println("How many numbers want to add in array");
+
 			int howMany = sc.nextInt();
 
 			int[] intArray = new int[howMany];
 
-			
+			System.out.println("Enter numbers ");			
+
+			for (int i=0 ;i < howMany ;i++) {
+
+
+					intArray[i]=(sc.nextInt());
+			}
+
+
+
+			Map<Integer,Integer> map = getSquares(intArray);
+
+			for (Map.Entry<Integer,Integer> entry : map.entrySet() ) {
+
+				System.out.println("Key   "+entry.getKey()+"   "+entry.getValue());
+				
+			}
+
 
 
 			System.out.println("Do you want to continue the process  press Y");
@@ -29,10 +49,26 @@ class SquareOfNumbers{
 
 
 	}
-	public static HashMap<Integer,Integer>getSquares(Integer[] integer){
+	public static HashMap<Integer,Integer>getSquares(int[] integer){
+
+		HashMap<Integer,Integer> map = new HashMap<>();
 
 
-		return square;
+		ArrayList<Integer>  list = new ArrayList<>();
+
+		for (Integer i  : integer ) {
+
+				list.add(i);
+
+		}
+
+
+		for (Integer j : list) {
+
+			map.put(j,(j*j));
+			
+		}
+		return map;
 
 	}
 
